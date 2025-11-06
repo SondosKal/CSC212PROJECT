6ab71d63239c507cb705d25e980bd9589ed2dcf6
SondosKal <sondosaldughaim@gmail.com>
2025-11-06 22:04:54 +0300

    Update Order.java

--- Order.java
+++ Order.java
@@ -1,56 +1,48 @@
-package project;
+package csc212project11;
+
+import java.time.LocalDate;
 
 public class Order {
 	private int orderId;
 	private int customerId;
-	private String prod_id;
+	private String prod_Ids;
 	private double totalPrice;
-	private String orderDate; // store as yyyy-MM-dd
+	private LocalDate orderDate; // store as yyyy-MM-dd
 	private String status; // pending, shipped, delivered, canceled
 	private LinkedList<Integer> productIds; // store product IDs in order
 	
 	
 	
-	public Order(int orderId, int customerId,String prod_id,double totalPrice, String orderDate, String status) {
+	public Order(int orderId, int customerId,String prod_ids,double totalPrice, LocalDate orderDate, String status) {
 		this.orderId = orderId;
 		this.customerId = customerId;
-		this.prod_id=prod_id;
+		this.prod_Ids=prod_ids;
 		this.totalPrice = totalPrice;
 		this.orderDate = orderDate;
 		this.status = status;
 		this.productIds = new LinkedList<Integer>();
-		addProductId(prod_id);
+		addIds(prod_ids);
 		}
 	
-	public void cancelOrder() {
-		status ="canceled";
-	}
-	
-	public void updateStatus(String newStatus) {
-        // Only allow valid statuses
-        if (newStatus.equals("pending") || newStatus.equals("shipped") ||
-            newStatus.equals("delivered") || newStatus.equals("canceled")) {
-            status = newStatus;
-        }
-    }
 	
 	
-	public void addProductId(String ids) {
+	public void addIds(String ids) {
 		String a[]= ids.split(";");
 		for(int i=0;i<a.length;i++)
 			productIds.addLast(Integer.parseInt(a[i].trim()));
 	}
 	
-	
 	public void addId(int id) {
 		productIds.addLast(id);
-	} 
+	}
+	
+	
 	
 	
 	public void updateOrder(Order ord) {
 		this.orderId = ord.orderId;
 		this.customerId = ord.customerId;
-		this.prod_id=ord.prod_id;
+		this.prod_Ids=ord.prod_Ids;
 		this.totalPrice = ord.totalPrice;
 		this.orderDate = ord.orderDate;
 		this.status = ord.status;
@@ -58,58 +50,25 @@ public class Order {
 	}
 	
 	
+
 	
-	
-	
-	
-	
-	
-	
-	
+
 	//SETTERS AND GETTERS
 
 	public int getOrderId() {return orderId;}
 
-	public int getCustomerId() {
-		return customerId;
-	}
-	public String getprod_id() {return prod_id; }
+	public int getCustomerId() {return customerId;}
+	public String getprod_Ids() {return prod_Ids; }
 
-	public LinkedList<Integer> getProductIds() {
-		return productIds;
-	}
+	public LinkedList<Integer> getProductIds() {return productIds;}
 
-	public double getTotalPrice() {
-		return totalPrice;
-	}
+	public double getTotalPrice() {return totalPrice;}
 
-	public String getOrderDate() {
-		return orderDate;
-	}
+	public LocalDate getOrderDate() {return orderDate;}
 
-	public String getStatus() {
-		return status;
-	}
+	public String getStatus() {return status;}
 
-	public void setOrderId(int orderId) {
-		this.orderId = orderId;
-	}
-
-	public void setCustomerId(int customerId) {
-		this.customerId = customerId;
-	}
-
-	public void setProductIds(LinkedList<Integer> productIds) {
-		this.productIds = productIds;
-	}
-
-	public void setTotalPrice(double totalPrice) {
-		this.totalPrice = totalPrice;
-	}
-
-	public void setOrderDate(String orderDate) {
-		this.orderDate = orderDate;
-	}
+	
 
 	public void setStatus(String status) {
 		this.status = status;
@@ -117,15 +76,18 @@ public class Order {
 	
 	
 	public void display() {
-		System.out.println();
+
+		System.out.println("Order ID: "+orderId);
+		System.out.println("Customer ID: "+customerId);
+		System.out.println("Product IDs: ");
+		productIds.display();
+		System.out.println("");
+		System.out.println("Total Price: "+totalPrice);
+		System.out.println("Date: "+orderDate);
+		System.out.println("Status: "+status);
+		System.out.println("------------------------------------");
+
 	}
-	
-	
-	
-	
-	
-	
-	
-	
+
 
 }
